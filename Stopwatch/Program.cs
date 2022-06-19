f5480a2ccb1a000443dfd0cfc934016063bc48ed
KarolDop <88980259+KarolDop@users.noreply.github.com>
2022-06-19 13:04:15 +0200

    Add files via upload

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,41 @@
+﻿
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var stopwatch = new Stopwatch();
+        var inputRead = "";
+        var menu = 4;
+        var isContinue = true;
+        do
+        {
+            Console.WriteLine("This application simulate stopwatch run. Press:" +
+                "\n1) Start" +
+                "\n2) Stop" +
+                "\n3) Exit");
+            inputRead = Console.ReadLine();
+            Int32.TryParse(inputRead, out menu);
+
+            if (!(Int32.TryParse(inputRead, out menu)) || !(menu == 1 || menu == 2 || menu == 3))
+            {
+                Console.WriteLine("Invalid data. Try once agian");
+                continue;
+            }
+
+            if (menu == 1)
+                stopwatch.Start();
+            else if (menu == 2)
+            {
+                stopwatch.Stop();
+                Console.WriteLine();
+                stopwatch.Result();
+            }
+            else if (menu == 3)
+                isContinue = false;
+
+            Console.WriteLine();
+            Console.WriteLine();
+        } while (isContinue);
+        
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Stopwatch
+{
+    private TimeSpan messuer;
+    private DateTime start;
+    private DateTime stop;
+    private bool isRun = false;
+    private int both = 0;
+
+
+
+    public void Start()
+    {
+        try
+        {
+            if (isRun == true)
+                throw new InvalidOperationException();
+            start = DateTime.Now;
+            isRun = true;
+            both++;
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public void Stop()
+    {
+        try
+        {
+            if(isRun == false)
+                throw new InvalidOperationException();
+            stop = DateTime.Now;
+            messuer = stop - start;
+            isRun = false;
+            both++;
+        }
+        catch(InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public void Result()
+    {
+        if (both == 2)
+        {
+            Console.WriteLine($"Hours: {messuer.Hours};\tMinutes: {messuer.Minutes};\tSeconds: {messuer.Seconds};\tMiliseconds: {messuer.Milliseconds}");
+            both = 0;
+        }
+    }
+}
