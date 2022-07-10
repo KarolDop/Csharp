9b04ebc2ad6404d4b67078c3596b2e912b649959
KarolDop <88980259+KarolDop@users.noreply.github.com>
2022-07-10 12:26:51 +0200

    Add files via upload

---
+++ Post/Post.cs
@@ -0,0 +1,31 @@
+﻿public class Post
+{
+    private int grade = 0;
+
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; private set; }
+
+    public Post(string title, string descripton)
+    {
+        Title = title;
+        Description = descripton;
+        CreatedDate = DateTime.Now;
+    }
+
+
+    public void gradeUp()
+    {
+        grade++;
+    }
+
+    public void gradeDown()
+    {
+        grade--;
+    }
+
+    public int getGrade()
+    {
+        return grade;
+    }
+}
---
+++ Post/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var post = new Post("Tytuł", "Krótki opis dla tego posta");
+
+        Console.WriteLine(post.CreatedDate);
+        Console.WriteLine(post.Description);
+        Console.WriteLine(post.Title);
+
+        post.Description = "Zmiana opisu";
+
+        Console.WriteLine(post.Description);
+    }
+}
