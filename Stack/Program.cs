78aaf91b1902924bad2c0afc440f9656c890107d
KarolDop <88980259+KarolDop@users.noreply.github.com>
2022-07-10 12:28:08 +0200

    Add files via upload

---
+++ Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        var stack = new Stack();
+        
+
+        var from = 0;
+        var to = 5;
+
+        for(int i = from; i < to; i++)
+            stack.Push(i);
+
+        for(int i = from; i < to; i++)
+            Console.WriteLine(stack.Pop());
+
+        for (int i = from; i < to; i++)
+            stack.Push(i);
+
+        stack.Clear();
+        
+        Console.WriteLine(stack.Pop());
+
+        stack.Push(null);
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,41 @@
+﻿public class Stack
+{
+    private readonly List<object> _stack = new List<object>();
+
+    public void Push(object obj)
+    {
+        try
+        {
+            if (obj != null)
+                _stack.Insert(0, obj);
+            else
+                throw new InvalidOperationException();
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine("Null can't be add to stack");
+        }
+    }
+
+    public void Clear()
+    {
+        _stack.Clear();
+    }
+
+    public object Pop()
+    {
+        try
+        {
+            if (_stack.Count == 0)
+                throw new InvalidOperationException();
+            var temp = _stack[0];
+            _stack.RemoveAt(0);
+
+            return temp;
+        }
+        catch (InvalidOperationException ex)
+        {
+            return "Stack is empty";
+        }
+    }
+}
